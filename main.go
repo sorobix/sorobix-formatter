736525d61f59c8bb942dbc432b027405bfe04427
Hemanth Krishna <hkpdev008@gmail.com>
2023-03-06 23:10:36 +0530

    init: sorobix-formatter
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+type request struct {
+	Code string `json:"code"`
+}
+
+func runFormatter(inputCode string) ([]byte, error) {
+	cmd := exec.Command("rustfmt")
+	cmd.Stdin = strings.NewReader(inputCode)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(out.String())
+	return out.Bytes(), nil
+}
+
+func base64Decoder(input string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(input)
+}
+
+func base64Encoder(input []byte) string {
+	return base64.StdEncoding.EncodeToString(input)
+}
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status":  "true",
+			"details": "sorobix-formatter",
+			"author":  "Hemanth Krishna <@DarthBenro008>",
+			"repository": "https://github." +
+				"com/sorobix/sorobix-formatter",
+		})
+	})
+	app.Post("/", func(c *fiber.Ctx) error {
+		var data request
+		err := c.BodyParser(&data)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(fiber.Map{
+				"error": "bad request",
+			})
+		}
+		input, err := base64Decoder(data.Code)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(fiber.Map{
+				"error": "bad base64 input",
+			})
+		}
+		result, err := runFormatter(string(input))
+		if err != nil {
+			c.Status(http.StatusNotAcceptable)
+			return c.JSON(fiber.Map{
+				"error": "bad rust code",
+				"logs":  err.Error(),
+			})
+		}
+		return c.JSON(fiber.Map{
+			"formatted_code": base64Encoder(result),
+		})
+	})
+	app.Listen(":3000")
+}
